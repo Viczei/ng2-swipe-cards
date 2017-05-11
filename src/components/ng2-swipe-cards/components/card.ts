81cbc8b62f830e80359d5ae67f74de3dcc6f1cb5
Victor Zeinstra <vzeinstra@santech.fr>
2017-05-11 14:02:26 +0100

    fix(card): replace calc(100%) by 100%

--- src/components/ng2-swipe-cards/components/card.ts
+++ src/components/ng2-swipe-cards/components/card.ts
@@ -55,9 +55,8 @@ import {
       opacity: 0;
       border-radius: 2px;
       position: absolute;
-      width: calc(100%);
+      width: 100%;
       height: 10px;
-      /*height: calc(100%);*/
       top: 0;
       left: 0;
       display: flex;
